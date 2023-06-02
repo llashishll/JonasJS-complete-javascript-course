64f22c5d26e26c575a5d717211114a26ef411d0d
llashishll <ashishkalra999@gmail.com>
2023-06-02 16:48:54 +0530

    Yolo
    latest commit

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+const showModalBtn = document.querySelectorAll(".show-modal");
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal')
+const overlay = document.querySelector('.overlay')
+
+for (let i = 0; i < showModalBtn.length; i++) {
+    showModalBtn[i].addEventListener('click', () => {
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+    })
+    console.log('clicked')
+}
+
+function closeModal() {
+
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+
+}
+
+closeModalBtn.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', (e) => e.key === 'Escape' ? closeModal() : console.log(e.key + ' is pressed'))
+overlay.addEventListener('click', closeModal)
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,35 @@
-'use strict';
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+
+// var x = 1;
+// const y = 2;
+// let z = 3;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+
+// console.log(window.x)
+// console.log(window.y)
+// console.log(window.z)
+
+// function addNum() {
+//     msg++
+// }
+
+// console.log(addNum())
+
+// let msg = 0;
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+var x = 1
+var y = 2
+var z = 3
+
+console.log(global)
+
